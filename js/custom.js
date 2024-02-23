26bb5c86237fc11b65e685690ead21b9ad92c359
JoseMiltonGuambi <145441268+JoseMiltonGuambi@users.noreply.github.com>
2024-02-23 16:34:54 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+    });
+
+    // NAVBAR
+    $(".navbar").headroom();
+
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
+
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
+    });
+
+})(window.jQuery);
+
+
+
+$(window).on('load', function () {
+    $('.logo-carousel').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: true,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
